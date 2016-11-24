6cccbfab4f0b67da9f1e7a9e939a77a54cb46cdb
Benoit Pierre <benoit.pierre@gmail.com>
2016-11-24 22:14:38 +0100

    pypy

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: User Interfaces',
